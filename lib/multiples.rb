556b0823b17e95d4e3c38f8be64eb5024186568e
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-05-08 12:00:55 -0400

    Euler

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  arr = []
+  for i in 1...limit
+    arr << i if i % 3 == 0 || i % 5 == 0
+  end
+  arr
+end
+
+def sum_multiples(limit)
+  arr = collect_multiples(limit)
+  arr.reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+    @multiples = []
+  end
+
+  def collect_multiples
+    for i in 1...@limit
+      @multiples << i if i % 3 == 0 || i % 5 == 0
+    end
+    @multiples
+  end
+
+  def sum_multiples
+    self.collect_multiples
+    @multiples.reduce(:+)
+  end
+
+end
